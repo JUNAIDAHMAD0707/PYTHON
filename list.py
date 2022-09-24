780e93db214b97a6f7357e56c035be0bb7f3a160
JUNAIDAHMAD0707 <114289480+JUNAIDAHMAD0707@users.noreply.github.com>
2022-09-24 16:26:09 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+items=[4,2.5,"junaid","two","two","three"]
+print(type(items))
+print(items[2])
+items[0:1]="salma","hassan"
+print(items)
+items[0:1]="two","three"
+print(items)
+print(items.count("two"))
+items[0]=4
+items[1]="salma"
+print(items)
+print(items.index("two"))
+
---
+++ strings.py
@@ -0,0 +1,19 @@
+name='''owais is a good student
+   he is currently studying at baba ghulam shah university'''
+print(name[-7])
+print(name[::2])
+print(len(name))
+print(name.count(" university "))
+print(name.count(" university"))
+print(name.count("university"))
+print(name.find("h"))#gives first index of h
+print(name.endswith("sity"))
+print(name.replace("university","badshah university"))
+"""fname=input("enter your name")
+print(fname.replace("dear ","junaid"))
+date=input("enter current date")
+print("current Date is:",date)"""
+Name =input("enter ur name")
+print(name.replace("owais",Name))
+print(name.count(" "))
+print(name)
