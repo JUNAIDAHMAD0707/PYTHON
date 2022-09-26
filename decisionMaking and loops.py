1dab3a4370d0ea095a4fe012b88658203caf210a
JUNAIDAHMAD0707 <114289480+JUNAIDAHMAD0707@users.noreply.github.com>
2022-09-26 17:10:35 +0530

    Add files via upload

---
+++ decisionMaking and loops.py
@@ -0,0 +1,36 @@
+from mimetypes import knownfiles
+
+
+var = 70
+#ElSE-IF ladder statement
+if(var==50):
+    print("The value of expression is 50")
+elif(var==60):
+    print ("The value of expression is 60")
+elif(var==70):
+    print ("The value of expression is 70")
+else:
+    print("The value of expression is greater than 70")
+#while loop
+while(var<=75):
+    print("the value of var is:",var)
+    var=var+1
+else:
+    print(var,"value is greater than 75")
+#for loop
+num = int(input("enter the number"))
+for j in range(2,num):
+    if(num%j==0):
+        flag=0
+        break
+    else:
+        flag=1
+if(flag==0):
+    print("entered number is not prime")
+if(flag==1):
+    print("entered number is prime")
+    
+
+
+    
+    
